64f2a89cb7cebe14a763d4bd729500ab7384147f
Lukman Nuriakhmetov <lookman.rays@gmail.com>
2023-12-18 21:08:08 +0100

    Init commit of grpc PoC — added open api response data in the local grpc server's response

--- test-grpc/ContentView.swift
+++ test-grpc/ContentView.swift
@@ -12,24 +12,61 @@ struct ContentView: View {
     let grpcClientManager = GrpcClientManager()
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text(responseTest)
+        ZStack {
+            // Background color
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+
+            // Content
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.blue)
+                    .padding(.top, 20)
+
+                Text("Hello, Norris!")
+                    .font(.headline)
+                    .padding(.top, 10)
+                    .padding(.bottom, 20)
+
+                Text(responseTest)
+                    .font(.body)
+                    .padding(.vertical, 10)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity, minHeight: 100)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+
+                Spacer() // Pushes everything above to the top
+
+                // Button at the bottom
+                Button(action: {
+                    performGrpcRequest()
+                }) {
+                    Text("Request Chuck Norris Fact")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                        .shadow(radius: 5)
+                }
+                .padding(.bottom)
+            }
         }
-        .padding()
         .onAppear {
             performGrpcRequest()
         }
     }
 
     private func performGrpcRequest() {
-        grpcClientManager.performRequest(message: "Hello Server") { response in
+        grpcClientManager.performRequest(message: "Norris") { response in
             DispatchQueue.main.async {
                 if let reply = response {
-                    self.responseTest = "Received reply: \(reply)"
+                    self.responseTest = "Received \(reply)"
                 } else {
                     self.responseTest = "Failed to receive reply"
                 }
--- test-grpc/MyServiceProvider.swift
+++ test-grpc/MyServiceProvider.swift
@@ -18,12 +18,71 @@ class MyServiceImpl: MyServiceProvider {
     var interceptors: MyServiceServerInterceptorFactoryProtocol? {
         return nil
     }
-
-    // Implement the methods defined in the MyServiceProvider protocol
+    
     func myMethod(request: MyRequest, context: StatusOnlyCallContext) -> EventLoopFuture<MyResponse> {
-        let response = MyResponse.with {
-            $0.reply = "Hello, \(request.message) 12345 Blah-blah-blah"
+        // Create a promise that you'll return
+        let promise = context.eventLoop.makePromise(of: MyResponse.self)
+        
+        // Example GET Request
+        performGetRequest { data, error in
+            guard let data = data, error == nil else {
+                promise.fail(error ?? GRPCStatus(code: .internalError, message: "Failed to perform GET request"))
+                return
+            }
+            
+            // Process data and create a response
+            let responseString = self.parseResponseData(data) ?? ""
+            let response = MyResponse.with {
+                $0.reply = "\(request.message)'s Response: \n \(responseString)"
+            }
+            
+            promise.succeed(response)
         }
-        return context.eventLoop.makeSucceededFuture(response)
+        
+        // Return the future result of the promise
+        return promise.futureResult
+    }
+    
+    private func performGetRequest(completion: @escaping (Data?, Error?) -> Void) {
+        // Define the URL for the GET request
+        guard let url = URL(string: "https://api.chucknorris.io/jokes/random") else {
+            completion(nil, GRPCStatus(code: .internalError, message: "Invalid URL"))
+            return
+        }
+        
+        // Create a URLSession data task
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            completion(data, error)
+        }
+        
+        // Start the task
+        task.resume()
+    }
+    
+    private func parseResponseData(_ data: Data) -> String? {
+        let decoder = JSONDecoder()
+        do {
+            let apiResponse = try decoder.decode(ApiResponse.self, from: data)
+            return apiResponse.value // This is the "value" field from the JSON
+        } catch {
+            print("Error decoding JSON: \(error)")
+            return nil
+        }
     }
 }
+
+struct ApiResponse: Decodable {
+    let iconUrl: String
+    let id: String
+    let url: String
+    let value: String
+
+    // CodingKeys enum maps the JSON field names to your struct's properties
+    enum CodingKeys: String, CodingKey {
+        case iconUrl = "icon_url"
+        case id
+        case url
+        case value
+    }
+}
+
